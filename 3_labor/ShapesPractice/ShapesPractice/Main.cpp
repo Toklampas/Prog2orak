7a9123c72976c5b706d57bde493b2d330524de97
Toklampas <161125099+Toklampas@users.noreply.github.com>
2025-03-04 14:44:11 +0100

    a

--- 3_labor/ShapesPractice/ShapesPractice/Main.cpp
+++ 3_labor/ShapesPractice/ShapesPractice/Main.cpp
@@ -5,6 +5,42 @@
 
 int main()
 {
+	printf("Testing Rectangle...\n");
+	Rectangle r1;
+	Rectangle r2(4);
+	Rectangle r3(2, 5);
+	r1.print(); // a=0.00, b=0.00 
+	r2.print(); // a=4.00, b=4.00 
+	r3.print(); // a=2.00, b=5.00 
+	printf("Area of r1=%.2lf\n", r1.getArea()); // Area of r1=0.00 
+	printf("Area of r2=%.2lf\n", r2.getArea()); // Area of r2=16.00 
+	printf("Area of r3=%.2lf\n", r3.getArea()); // Area of r3=10.00 
+	printf("Perimeter of r1=%.2lf\n", r1.getPerimeter()); // Perimeter of r1=0.00 
+	printf("Perimeter of r2=%.2lf\n", r2.getPerimeter()); // Perimeter of r2=16.00 
+	printf("Perimeter of r3=%.2lf\n", r3.getPerimeter()); // Perimeter of r3=14.00 
+	r3.seta(r2.geta());
+	r3.setb(r2.getb());
+	r3.print(); // a=4.00, b=4.00 
+	printf("Area of r3=%.2lf\n", r3.getArea()); // Area of r3=16.00 
+	printf("Perimeter of r3=%.2lf\n", r3.getPerimeter()); // Perimeter of r3=16.00 
+
+
+	printf("\nTesting Circle...\n");
+	Circle c1;
+	Circle c2(10);
+	c1.print(); // r=0.00 
+	c2.print(); // r=10.00 
+	printf("Area of c1=%.2lf\n", c1.getArea()); // Area of c1 = 0.00 
+	printf("Area of c2=%.2lf\n", c2.getArea()); // Area of c2 = 314.16 
+	printf("Circumference of c1=%.2lf\n", c1.getCircumference()); // Circumference of c1 = 0.00 
+	printf("Circumference of c2=%.2lf\n", c2.getCircumference()); // Circumference of c2 = 62.83 
+	c1.setr(c2.getr());
+	c1.print();
+	printf("Radius of c1=%.2lf\n", c1.getr()); // Radius of c1=10.00 
+	printf("Area of c1=%.2lf\n", c1.getArea()); // Area of c1=314.16 
+	printf("Circumference of c1=%.2lf\n", c1.getCircumference()); // Circumference of c1=62.83
+
+
 	printf("\nTesting BoundingCircle...\n");
 	BoundingCircle b1;
 	BoundingCircle b2(10);
