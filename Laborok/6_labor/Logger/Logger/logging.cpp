0c114662212f8476db61e63e3e3339cf138ec93b
Albrecht Ádám <161125099+Toklampas@users.noreply.github.com>
2025-03-19 15:44:27 +0100

    Add files via upload

---
+++ Laborok/6_labor/Logger/Logger/logging.cpp
@@ -0,0 +1,23 @@
+﻿#include "logging.h"
+
+using namespace logging;
+
+Logger::Logger()
+{}
+
+Logger::Logger(const Logger& theOther)
+{}
+
+Logger& Logger::getInstance()
+{
+	static Logger theLogger; //statikus, tehat egy peldany fog belole letezni
+	return theLogger;
+}
+
+void logging::Logger::setDefaultLogLevel(const LogLevel&)
+{
+}
+
+void logging::Logger::log(LogLevel, const char*)
+{
+}
