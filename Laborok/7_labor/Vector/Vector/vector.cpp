4b4600489aaec795888ad37f22fba9851bc8bf44
Toklampas <161125099+Toklampas@users.noreply.github.com>
2025-03-28 21:25:13 +0100

    hetedik imsc feladat megoldas

--- Laborok/7_labor/Vector/Vector/vector.cpp
+++ Laborok/7_labor/Vector/Vector/vector.cpp
@@ -4,36 +4,12 @@
 #include <iostream>
 using namespace std;
 
-Vector& Vector::operator=(const Vector& theOther)
-{
-    if (this == &theOther) //megegyezik e az osztalypeldam azzal amibol masolok, ha egyenlok segmentation fault lenne ha nem foglalkoznank vele
-        return *this;
-    delete[] pData;
-    maxElementsNum = theOther.maxElementsNum;
-    if (!theOther.elementNum)
-    {
-        elementNum = 0;
-        pData = nullptr;
-    }
-    else
-    {
-        elementNum = theOther.elementNum;
-        pData = new int[theOther.elementNum]; //kell default konstruktor
-        for (unsigned i = 0; i < theOther.elementNum; i++)
-        {
-            pData[i] = theOther.pData[i];
-        }
-
-    }
-    return *this; //benne van az osztalypeldany
-}
-
 Vector::Vector(unsigned maxElementsNum) : elementNum(0), maxElementsNum(maxElementsNum), pData(nullptr) {}
 
 Vector::Vector(const Vector& theOther)
 {
-    Vector uj;
-    uj = theOther;
+    pData = nullptr;
+    *this = theOther;
 }
 
 Vector::~Vector()
@@ -128,15 +104,6 @@ bool Vector::insert(unsigned position, int element)
     return true;
 }
 
-
-
-std::ostream& operator<<(std::ostream& os, const Vector& v)
-{
-    for (int i = 0; i < v.elementNum; i++)
-        os << v.pData[i] << " ";
-    return os;
-}
-
 int& Vector::at(unsigned position)
 {
     if (position >= 0 && position <= elementNum)
@@ -147,8 +114,8 @@ const int& Vector::at(unsigned position) const
 {
     if (position >= 0 && position <= elementNum)
     {
-        const int eredmeny = pData[position];
-        return pData[position];
+        const int out = pData[position];
+        return out;
     }
 }
 
@@ -159,50 +126,107 @@ int& Vector::operator[](unsigned position)
 
 const int& Vector::operator[](unsigned position) const
 {
-    const int eredmeny = at(position);
-    return eredmeny;
-}
-
-void Vector::operator*=(unsigned right)
-{
-    for (unsigned i = 0; i < elementNum; i++)
-    {
-        pData[i]*= right;
-    }
-
-}
-
-void Vector::operator+=(unsigned right)
-{
-    for (unsigned i = 0; i < elementNum; i++)
-    {
-        pData[i] += right;
-    }
-}
-
-istream& operator>>(istream& is, Vector& v)
-{
-    for (unsigned i = 0; i < v.elementNum; i++)
-    {
-        is >> v.pData[i];
-    }
-    return is;
+    return at(position);
 }
 
 bool Sorters::ascend(const int& a, const int& b)
 {
-    return b > a;
+    return a < b;
 }
 
 bool Sorters::descend(const int& a, const int& b)
 {
-    return b < a;
+    return a > b;
 }
 
 void Vector::sort(bool(*predicate)(const int& a, const int& b))
 {
-   
+    for (unsigned i = 0; i < elementNum - 1; i++)
+    {
+        unsigned minIndex = i;
+        for (unsigned j = i + 1; j < elementNum; j++)
+            if (predicate(pData[j], pData[minIndex]))
+                minIndex = j;
+        std::swap(pData[i], pData[minIndex]);
+    }
 }
 
+Vector& Vector::operator=(const Vector& theOther)
+{
+    if (this == &theOther)
+        return *this;
+    delete[] pData;
+    maxElementsNum = theOther.maxElementsNum;
+    if (!theOther.elementNum)
+    {
+        elementNum = 0;
+        pData = nullptr;
+    }
+    else
+    {
+        elementNum = theOther.elementNum;
+        pData = new int[elementNum];
+        for (unsigned i = 0; i < elementNum; i++)
+        {
+            pData[i] = theOther.pData[i];
+        }
 
+    }
+    return *this;
+}
 
+std::ostream& operator<<(std::ostream& os, const Vector& v)
+{
+    for (unsigned i = 0; i < v.elementNum; i++)
+        os << v.pData[i] << " ";
+    return os;
+}
+
+std::istream& operator>>(std::istream& is, Vector& v)
+{
+
+    for (int i = 0; i < v.maxElementsNum; i++)
+    {
+
+        int input;
+        is >> input;
+        if (is.good())
+        {
+            v.elementNum++;
+            int* temp = new int[v.elementNum];
+
+            if (v.pData)
+                for (int j = 0; j < v.elementNum; j++)
+                    temp[j] = v.pData[j];
+
+            temp[i] = input;
+            delete[] v.pData;
+            v.pData = temp;
+        }
+        else
+            return is;
+    }
+    return is;
+}
+
+void Vector::operator*=(unsigned right)
+{
+    if (pData == nullptr || elementNum == 0)
+        return;
+    else
+    {
+        for (unsigned i = 0; i < elementNum; i++)
+            pData[i] *= right;
+    }
+}
+
+void Vector::operator+=(unsigned right)
+{
+    if (pData == nullptr || elementNum == 0)
+        return;
+    else
+    {
+        for (unsigned i = 0; i < elementNum; i++)
+            pData[i] += right;
+    }
+}
