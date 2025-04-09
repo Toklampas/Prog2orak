9cdbd48757af9ed595042d4dc5cdd1ee51f3761e
Albrecht Ádám <161125099+Toklampas@users.noreply.github.com>
2025-04-09 15:05:12 +0200

    equipment added

---
+++ Laborok/Equipment/Equipment/display.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Equipment.h"
+
+
+//Displayeket és Printereket közös tömbben szeretnénk valahogy tárolni
+class Display : public Equipment
+{
+	unsigned age;
+public:
+	Display(unsigned serialNumber, unsigned price, unsigned age);
+
+	void setAge(unsigned age);
+	unsigned getAge() const;
+
+	void print() const;
+};
